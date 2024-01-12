9ddfc9e8fd38e87d77e8e7b93f33f5880fd39ac9
Cristiano Souza <ltianosouzal@outlook.com>
2024-01-11 21:10:55 -0300

    Commit initial: config app

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,53 @@
+import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from "stimulus-use"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ["content"]
+  connect() {
+    useClickOutside(this)
+    // this.close()
+  }
+
+  clickOutside(event) {
+    this.close()
+  }
+
+  closeWithKeyboard(event) {
+    if (event.key === "Escape") {
+      this.close()
+    }
+  }
+
+  closeOnBigScreen(event) {
+    if (window.innerWidth > 768) {
+      this.close()
+    }
+  }
+
+  toggle() {
+    if (this.contentTarget.classList.contains('hidden')) {
+      this.open()
+    } else {
+      this.close()
+    }
+  }
+
+  open() {
+    this.contentTarget.classList.remove('hidden')
+    let main = document.querySelector('main')
+    main.classList.add('blur')
+    document.body.classList.add('overflow-hidden')
+    // main.classList.add('hidden')
+  }
+
+  close() {
+    this.contentTarget.classList.add('hidden')
+    let main = document.querySelector('main')
+    main.classList.remove('blur')
+    document.body.classList.remove('overflow-hidden')
+    // main.classList.remove('hidden')
+  }
+
+  
+}
